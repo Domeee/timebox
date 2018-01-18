39b123bbda511b1c3efb3b4510ab3c12d4bab6c2
Dominique Huber <dominique@donhubi.ch>
2018-01-18 17:44:54 +0100

    Implement themes

--- src/components/SoundSelection.tsx
+++ src/components/SoundSelection.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react';
 import SoundChangeEvent from '../lib/SoundChangeEvent';
 
-import closeWhite from './close-white.svg';
-import dropDownCaret from './dropdown-caret.svg';
-
 import './SoundSelection.css';
 
 export interface SoundSelectionProps {
@@ -43,7 +40,7 @@ class SoundSelection extends React.Component<
   public render() {
     const soundItems = this.sounds.map(s => {
       return (
-        <li key={s.name} className="sound-item">
+        <li key={s.name} className="sound-item color-focus">
           <input
             type="radio"
             name="sound"
@@ -64,7 +61,29 @@ class SoundSelection extends React.Component<
       <React.Fragment>
         <div className="sound-selection-toggle" onClick={this.toggleOverlay}>
           {sound.title}
-          <img src={dropDownCaret} />
+          <svg
+            className="sound-selection-toggle-icon"
+            viewBox="0 0 13 8"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+          >
+            <g
+              stroke="none"
+              strokeWidth="1"
+              fill="none"
+              fillRule="evenodd"
+              transform="translate(-911.000000, -82.000000)"
+            >
+              <g transform="translate(693.000000, 64.000000)">
+                <path
+                  // tslint:disable-next-line:max-line-length
+                  d="M221.652,16.152 L221.652,23.8836923 C221.652,24.0674585 221.393952,24.216 221.076,24.216 C220.758048,24.216 220.5,24.0674585 220.5,23.8836923 L220.5,15.9083077 C220.5,15.8727099 220.509683,15.8384339 220.527609,15.8063034 C220.509848,15.7357619 220.5,15.6578667 220.5,15.576 C220.5,15.258048 220.648542,15 220.832308,15 L228.807692,15 C228.991458,15 229.14,15.258048 229.14,15.576 C229.14,15.893952 228.991458,16.152 228.807692,16.152 L221.652,16.152 Z"
+                  transform="translate(224.820000, 19.608000) rotate(-135.000000) translate(-224.820000, -19.608000) "
+                />
+              </g>
+            </g>
+          </svg>
         </div>
         <div
           className="overlay"
@@ -73,12 +92,42 @@ class SoundSelection extends React.Component<
           }}
         >
           <div className="overlay-close-container">
-            <img
-              src={closeWhite}
+            <svg
               className="overlay-close"
-              alt="Timebox Toggle"
               onClick={this.toggleOverlay}
-            />
+              viewBox="0 0 36 36"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              xmlnsXlink="http://www.w3.org/1999/xlink"
+            >
+              <g
+                stroke="none"
+                strokeWidth="1"
+                fill="none"
+                fillRule="evenodd"
+                transform="translate(-494.000000, -85.000000)"
+              >
+                <g
+                  transform="translate(494.000000, 85.000000)"
+                  className="overlay-close-g"
+                >
+                  <path
+                    // tslint:disable-next-line:max-line-length
+                    d="M18,0 C8.07492857,0 0,8.07492857 0,18 C0,27.9250714 8.07492857,36 18,36 C27.9250714,36 36,27.9250714 36,18 C36,8.07492857 27.9250714,0 18,0 Z M18,34.7142857 C8.78335714,34.7142857 1.28571429,27.216 1.28571429,18 C1.28571429,8.784 8.78335714,1.28571429 18,1.28571429 C27.2166429,1.28571429 34.7142857,8.784 34.7142857,18 C34.7142857,27.216 27.2166429,34.7142857 18,34.7142857 Z"
+                  />
+                  <path
+                    // tslint:disable-next-line:max-line-length
+                    d="M25.7142857,17.3571429 L10.2857143,17.3571429 C9.93021429,17.3571429 9.64285714,17.6451429 9.64285714,18 C9.64285714,18.3548571 9.93021429,18.6428571 10.2857143,18.6428571 L25.7142857,18.6428571 C26.0697857,18.6428571 26.3571429,18.3548571 26.3571429,18 C26.3571429,17.6451429 26.0697857,17.3571429 25.7142857,17.3571429 Z"
+                    transform="translate(18.000000, 18.000000) rotate(-45.000000) translate(-18.000000, -18.000000) "
+                  />
+                  <path
+                    // tslint:disable-next-line:max-line-length
+                    d="M25.7142857,17.3571429 L10.2857143,17.3571429 C9.93021429,17.3571429 9.64285714,17.6451429 9.64285714,18 C9.64285714,18.3548571 9.93021429,18.6428571 10.2857143,18.6428571 L25.7142857,18.6428571 C26.0697857,18.6428571 26.3571429,18.3548571 26.3571429,18 C26.3571429,17.6451429 26.0697857,17.3571429 25.7142857,17.3571429 Z"
+                    transform="translate(18.000000, 18.000000) rotate(45.000000) translate(-18.000000, -18.000000) "
+                  />
+                </g>
+              </g>
+            </svg>
           </div>
           <div className="overlay-content">
             <form>
--- src/components/TimeboxToggle.tsx
+++ src/components/TimeboxToggle.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import playWhite from './play-white.svg';
-import stopWhite from './stop-white.svg';
 
 import './TimeboxToggle.css';
 
@@ -16,15 +14,47 @@ class TimeboxToggle extends React.Component<TimeboxToggleProps> {
   }
 
   public render() {
-    const svg = this.props.isTimeboxStarted ? stopWhite : playWhite;
+    const play = (
+      <g
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+        transform="translate(-148.000000, -507.000000)"
+      >
+        <g transform="translate(148.000000, 507.000000)">
+          <circle strokeWidth="4" cx="40" cy="40" r="38" />
+          <polygon points="32 22 32 58 56 40" />
+        </g>
+      </g>
+    );
+    const stop = (
+      <g
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+        transform="translate(-148.000000, -505.000000)"
+      >
+        <g transform="translate(148.000000, 505.000000)">
+          <circle strokeWidth="4" cx="40" cy="40" r="38" />
+          <rect x="27" y="27" width="26" height="26" />
+        </g>
+      </g>
+    );
+    const svg = this.props.isTimeboxStarted ? stop : play;
 
     return (
-      <img
-        src={svg}
+      <svg
         className="timebox-toggle-icon"
-        alt="Timebox Toggle"
         onClick={this.handleTimeboxToggle}
-      />
+        viewBox="0 0 80 80"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+      >
+        {svg}
+      </svg>
     );
   }
 
